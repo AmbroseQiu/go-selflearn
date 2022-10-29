9f7689e385675e6c8cc21f9fce3ab0778232658f
AmbroseQiu <ambroseqiu1027@gmail.com>
2022-10-29 07:07:53 +0000

    add interface

--- golang_toturial/pack1/submodule.go
+++ golang_toturial/pack1/submodule.go
@@ -1,11 +1,61 @@
 package pack1
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func subpack2() {
-	fmt.Println("call subpack2")
+type Contact struct {
+	ID          string
+	PhoneNumber string
+	Gender      int
+}
+
+type Animal interface {
+	bark()
+}
+
+type Cat struct {
+	name string
+}
+
+type Dog struct {
+	name string
+}
+
+func (cat_ *Cat) setName() {
+	cat_.name = "cat"
+}
+
+func (dog_ *Dog) setName() {
+	dog_.name = "dog"
+}
+
+func (cat_ *Cat) bark() {
+	fmt.Println(cat_.name, " meow")
+}
+
+func (dog_ *Dog) bark() {
+	fmt.Println(dog_.name, " wan")
+}
+
+func animalBark(animal_ Animal) {
+	switch animal := animal_.(type) {
+	case *Cat:
+		animal.setName()
+	case *Dog:
+		animal.setName()
+	}
+	animal_.bark()
 }
 
 func CallFunc() {
-	subpack2()
+	contact := &Contact{
+		ID:          "Ambrose",
+		PhoneNumber: "1231",
+		Gender:      1,
+	}
+
+	fmt.Printf("ID=%v\n", contact.ID)
+	animal := Cat{}
+	animalBark(&animal)
 }
