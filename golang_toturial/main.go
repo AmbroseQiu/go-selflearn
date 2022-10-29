4c28f84c2d91cbf13fee100a517aa18b79889f92
AmbroseQiu <ambroseqiu1027@gmail.com>
2022-10-29 07:08:11 +0000

    add code

--- golang_toturial/main.go
+++ golang_toturial/main.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	"fmt"
-
-	subpack2 "example.com/golang_tutorial/pack1"
-	subpack "example.com/golang_tutorial/pack2"
+	subpack "example.com/golang_tutorial/pack1"
+	subpack2 "example.com/golang_tutorial/pack2"
 )
 
-func init() {
-	fmt.Println("init in main 1")
-}
+// func init() {
+// 	fmt.Println("init in main 1")
+// }
 
-func init() {
-	fmt.Println("init in main 2")
-}
+// func init() {
+// 	fmt.Println("init in main 2")
+// }
 
-func init() {
-	fmt.Println("init in main 3")
-}
+// func init() {
+// 	fmt.Println("init in main 3")
+// }
 
 func main() {
-	fmt.Println("hello program")
-	subpack.CallG()
-	subpack2.CallFunc()
-	subpack.Print2DArray()
+	subpack2.CallG()
+	subpack.CallFunc()
+	// subpack.Print2DArray()
 }
--- golang_toturial/pack2/submodule.go
+++ golang_toturial/pack2/submodule.go
@@ -54,5 +54,43 @@ func Print2DArray() {
 		}
 		fmt.Println()
 	}
+	key := selectByKey("please", "use", "this", "func")
+	fmt.Println("key = ", key)
+	tryMap()
+}
 
+func selectByKey(text ...string) (key int) {
+	for i, v := range text {
+		fmt.Printf("%v: %s\n", i+1, v)
+	}
+	fmt.Print("type key: ")
+	// fmt.Scanln(&key)
+	return key
+}
+
+func tryMap() {
+	var m1 map[string]string
+	fmt.Println("m1 == nil ? ", m1 == nil)
+
+	m1 = make(map[string]string, 2)
+
+	m1["how r u"] = "I fine thank u"
+
+	m1["a for apple"] = "b for ball"
+
+	for key, value := range m1 {
+		fmt.Printf("key=%v,value=%v\n", key, value)
+	}
+
+	value, ret := m1["who are u"]
+	if ret {
+		fmt.Println(value)
+	} else {
+		fmt.Println("Ambrose")
+	}
+
+	delete(m1, "a for apple")
+	for key, value := range m1 {
+		fmt.Printf("key=%v,value=%v\n", key, value)
+	}
 }
